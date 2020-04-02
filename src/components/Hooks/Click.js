d309d33901651cff9f2d5696568b9529a70ffe5f
Abhishek Nag <nag.abhishek2012@gmail.com>
2020-04-02 18:14:22 +0530

    Practice

--- src/components/Hooks/Click.js
+++ src/components/Hooks/Click.js
@@ -6,9 +6,14 @@ function Click() {
 
     const [count, setCount] = useState(0);
     const [hideFlag, setToggle] = useState(true);
-    const userName = useContext(UserContext);
-    const eleRef = useRef();
+    const [time, setTime] = useState(new Date().toString());
+    const [isTimerOn,toggleTimerFlag] = useState(false);
 
+    const eleRef = useRef();
+    let timerRef = useRef();
+    let initData = 10;
+
+    const userName = useContext(UserContext);
 
     const incrementCount = () => {
         setCount(prevState => prevState + 1);
@@ -17,10 +22,14 @@ function Click() {
     const toggleContent = () => {
         setToggle(prevState => !prevState);
     }
-
+    
     useEffect(() => {
         console.log("Init App");
         eleRef.current.style.color = 'green';
+        timerRef.current = setInterval(() => {
+            toggleTimerFlag(false);
+            setTime(new Date().toString()) 
+        },1000);
         return () => {console.log('Cleanup App')};
     }, []);
 
@@ -29,18 +38,36 @@ function Click() {
         return () => {console.log('Cleanup Count')};
     }, [count]);
 
+    const cancelTimer = () => {
+        toggleTimerFlag(true);
+        clearInterval(timerRef.current);
+    }
+
+    const startTimer = () => {
+        timerRef.current = setInterval(() => {
+            setTime(new Date().toString());
+            toggleTimerFlag(false);
+        },1000);
+    }
+
     return (
         <div>
+            <h3>useContext Example</h3>
             <UserContext.Consumer>
-                {
-                    (userName) => <div>User name from UserContext.Consume: {userName}</div>
-                }
+                { (userName) => <div>User name from UserContext.Consumer: {userName}</div> }
             </UserContext.Consumer>
             <div>User name from useContext: {userName}</div>
+            
+            <h3>useState Example</h3>
             <div ref={eleRef}>{ hideFlag ? <div>Counter: {count} </div> : null }</div>
             <button onClick={incrementCount}>Increment count</button>
-            <br/>
             <button onClick={toggleContent}>Toggle</button>
+
+            <h3>useRef Example</h3>
+            <div>Current Time is {time}</div>
+            {
+                isTimerOn ?  <button onClick={startTimer}>Start Seconds Timer</button> : <button onClick={cancelTimer}>Stop Seconds Timer</button>
+            }
         </div>
     )
 
